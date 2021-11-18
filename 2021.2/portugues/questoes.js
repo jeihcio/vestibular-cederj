2d4f9d390a8c04d6c5ae6030af51f1a5ab748d8e
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-11-18 17:10:45 -0300

    add a primeira pergunta

--- 2021.2/portugues/questoes.js
+++ 2021.2/portugues/questoes.js
@@ -4,18 +4,62 @@
 
 quiz = {
     0: {
-        pergunta: ``,
+        pergunta: `<h3>Texto I</h3><br/>
+        A diferença entre ciência e fé é a seguinte: em ciência,
+        a gente tem que ver para crer. Você observa a natureza, você observa o mundo, obtém dados sobre como o
+        mundo funciona, analisa esses dados e entende. Pela
+        fé, você crê para ver. A crença vem antes da visão.
+        Você acredita naquilo, nem precisa ver nada, acredita
+        naquilo e esse, essencialmente, é o cerne da fé, que é
+        uma outra maneira de se relacionar com a realidade,
+        muito diferente da ciência.<br/>
+        Infelizmente, hoje em dia, parece que essa questão
+        está novamente a mil com a chamada ‘guerra’ entre
+        a ciência e a religião. Na verdade, essa é uma guerra
+        fabricada, porque, por exemplo, se você pergunta aos
+        cientistas, mais ou menos 40% deles, ao menos nos
+        Estados Unidos — não sei se existe essa estatística
+        no Brasil, talvez seja até maior aqui —, acreditam em
+        alguma forma de divindade, de Deus.<br/>
+        (...)<br/>
+        Para esses cientistas, existe um compromisso, uma
+        complementaridade entre o seu trabalho e a sua fé. Não
+        existe nenhum problema nesse caso. Mas, infelizmente,
+        existe conflito em outras situações.<br/>
+        (...)<br/>
+        A criança aprende numa aula que houve toda uma evolução da vida, os fósseis etc., 3,5 bilhões de anos de
+        evolução da vida aqui na Terra enquanto, na outra aula,
+        o professor diz que não. Que em seis dias Deus fez o
+        mundo, que nós somos todos descendentes de Adão e
+        Eva e o mundo tem apenas dez mil anos.<br/>
+        Note que a proposta é que isso seja ensinado em pé de
+        igualdade. São duas versões da mesma história e nenhuma é melhor do que a outra. Mas são, sim, duas histórias muito diferentes, com um objetivo muito diferente.
+        Então, a questão é como é construída a informação na
+        ciência.<br/>
+        (...) Não existe a possibilidade de um cientista afirmar:
+        eu acho que esse pedaço de osso aqui tem três milhões
+        de anos. Você sabe que tem três milhões de anos, com
+        grande precisão.<br/>
+        (...)<br/>
+        <small>Disponível em https://www.fronteiras.com/artigos/21-ideias-marcelo-gleiser-ea-complementaridade-entre-religiao-e-ciencia - adaptado. Acesso em: 05 de
+        maio de 2021</small>
+        <br/><br/>
+
+        <b>01.</b> O texto I estabelece uma relação entre ciência e
+            religião. De acordo com o autor, é possível afirmar que
+            tanto a ciência quanto a religião são
+        `,
 
         alternativas: {
             /* A */
-            0: '',
+            0: 'visões numéricas, logo precisam ser respeitadas.',
             /* B */
-            1: '',
+            1: 'pontos de vista equivalentes, portanto, indiscutíveis.',
             /* C */
-            2: '',
+            2: 'olhares distintos, porém sem o aspecto belicoso.',
             /* D */
-            3: '',
+            3: 'noções inviáveis como teorias universais, mas importantes.',
         },
-        resposta: -1
+        resposta: 2
     }
 }
