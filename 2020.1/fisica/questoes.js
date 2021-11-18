214c7425e5a2ff4ea141df54425a3bede8f3375d
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-11-18 16:43:44 -0300

    Criando os quis para os anos de 2021 e 2020

---
+++ 2020.1/fisica/questoes.js
@@ -0,0 +1,21 @@
+﻿var quiz = new Array(),
+    perguntasJaSaiu = new Array(),
+    contador = 0;
+
+quiz = {
+    0: {
+        pergunta: ``,
+
+        alternativas: {
+            /* A */
+            0: '',
+            /* B */
+            1: '',
+            /* C */
+            2: '',
+            /* D */
+            3: '',
+        },
+        resposta: -1
+    }
+}
